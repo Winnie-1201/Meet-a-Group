967f84cc289387d70d8a3a4f4512eab1bc734a50
Winnie <luwinnie12@gmail.com>
2022-09-22 16:53:20 -0400

    Add Users table migration and seeds

--- backend/db/seeders/20220922201823-demo-user.js
+++ backend/db/seeders/20220922201823-demo-user.js
@@ -1,7 +1,9 @@
-'use strict';
+"use strict";
+
+const bcrypt = require("bcryptjs");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -10,15 +12,44 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          email: "demo@user.io",
+          username: "Demo-lition",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+        {
+          email: "user1@user.io",
+          username: "FakeUser1",
+          hashedPassword: bcrypt.hashSync("password2"),
+        },
+        {
+          email: "user2@user.io",
+          username: "FakeUser2",
+          hashedPassword: bcrypt.hashSync("password3"),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      "Users",
+      {
+        username: { [Op.in]: ["Demo-lition", "FakeUser1", "FakeUser2"] },
+      },
+      {}
+    );
+  },
 };
