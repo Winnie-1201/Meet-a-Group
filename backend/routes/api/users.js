61cf3ccaf9c7d42ce06222f22bcfa2ea9a12c0b6
Winnie <luwinnie12@gmail.com>
2022-09-24 17:16:35 -0400

    update users api

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -27,8 +27,8 @@ const validateSignup = [
 ];
 // Sign up
 router.post("/", validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
-  const user = await User.signup({ email, username, password });
+  const { firstName, lastName, email, password } = req.body;
+  const user = await User.signup({ firstName, lastName, email, password });
 
   await setTokenCookie(res, user);
   return res.json({
