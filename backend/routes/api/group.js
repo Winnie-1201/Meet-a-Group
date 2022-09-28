e2ea5823cf86649dda7fded944ed132f808480b9
Winnie <luwinnie12@gmail.com>
2022-09-27 23:29:59 -0400

    debug get all groups joined or organized by the curr user

--- backend/routes/api/group.js
+++ backend/routes/api/group.js
@@ -163,7 +163,7 @@ router.get("/current", requireAuth, async (req, res, next) => {
     }
     const numMembers = await Membership.findAll({
       where: {
-        currUserId,
+        userId: currUserId,
         status: {
           [Op.in]: ["member", "co-host"],
         },
