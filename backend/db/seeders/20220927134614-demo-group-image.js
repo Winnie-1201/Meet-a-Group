2505b412b169003c03c3e8ed56afa72625481b1a
Winnie <luwinnie12@gmail.com>
2022-10-15 07:54:01 -0700

    create the component and reducer for getting all groups

--- backend/db/seeders/20220927134614-demo-group-image.js
+++ backend/db/seeders/20220927134614-demo-group-image.js
@@ -5,17 +5,17 @@
 const groupImages = [
   {
     groupId: 1,
-    url: "https://drive.google.com/file/d/1GVhu4nN0O_UwKBSC0jEtES-LcPLjIaJ4/view?usp=sharing",
+    url: "http://drive.google.com/uc?export=view&id=1mQx88tWT6d0t73_CN9xy8bwfvByyNHJT",
     preview: true,
   },
   {
     groupId: 2,
-    url: "https://drive.google.com/file/d/1bpiL3ALmKm3CRogwSGkqHMnRKrnm7VDm/view?usp=sharing",
+    url: "http://drive.google.com/uc?export=view&id=1FI6hmtrNCK_5EbbAS7o0Zc6lCcI1Xvr6",
     preview: true,
   },
   {
     groupId: 3,
-    url: "https://drive.google.com/file/d/1wGwNUwdMBEjM4x2MzRirh_lJ8XN7lVZB/view?usp=sharing",
+    url: "http://drive.google.com/uc?export=view&id=1wGwNUwdMBEjM4x2MzRirh_lJ8XN7lVZB",
     preview: true,
   },
   // {
