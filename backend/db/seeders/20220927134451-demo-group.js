c1b2e61ea39bfcfa0a52975429e135c7dd837c11
Winnie <luwinnie12@gmail.com>
2022-10-26 15:32:24 -0400

    update

--- backend/db/seeders/20220927134451-demo-group.js
+++ backend/db/seeders/20220927134451-demo-group.js
@@ -7,7 +7,7 @@ const groups = [
     organizerId: 1,
     name: "Mini Adventures For Free - Hiking, Dancing, Sports & More",
     about:
-      "Our meetups include hikes,biking, dancing,Classes, picnics, sport games, concerts, happy hour, and whatever else that attracts interest. Meet at bars, parks, baseball/basketball games, rec-leagues, or anywhere that sounds fun. Not just for singles, not just for couples... this is for EVERYONE who wants to get off the couch and want to enjoy what the greatest city has to offer without thinking much about your buget. We have a happy hour each month where we raise money for various local charities (all voluntary donations). I'm also looking for people to help organize events so let me know if you are interested...",
+      "Our meetups include hikes,biking, dancing,Classes, picnics, sport games, concerts, happy hour, and whatever else that attracts interest. Meet at bars, parks, baseball/basketball games, rec-leagues, or anywhere that sounds fun. Not just for singles, not just for couples... ",
     type: "In person",
     private: false,
     city: "Flagstaff",
@@ -17,7 +17,7 @@ const groups = [
     organizerId: 2,
     name: "San Francisco Pets and Fun Paint Party Meetup",
     about:
-      "Join us at Dog friendly restaurant Barking Dog on SF upper East side or in Central Park. How about an Online Sip and Paint Party with all your friends. Artist will email you a drawing of your pet or mail you the full Pet Portrait Fun Art Kit with all the supplies. Join the artist live through Zoom. Looking for ideas for a great Date Nite with your dog? Don't leave your dog at home, get out of the house, bring them with you for a night out in SF. Join me for a relaxing paint your pet paint nite with both your loved ones. One of the best date night ideas for both of you. Come paint your pet portrait have some wine and relax.",
+      "Join us at Dog friendly restaurant Barking Dog on SF upper East side or in Central Park. How about an Online Sip and Paint Party with all your friends. Artist will email you a drawing of your pet or mail you the full Pet Portrait Fun Art Kit with all the supplies.",
     type: "In person",
     private: false,
     city: "San Francisco",
--- backend/db/seeders/20220927134456-demo-event.js
+++ backend/db/seeders/20220927134456-demo-event.js
@@ -8,7 +8,7 @@ const events = [
     groupId: 1,
     name: "Free Webinar on Hiking Safety and Technology",
     description:
-      "Join experts from Montrose Search & Rescue (SAR) and Sierra Club's Wilderness Travel Course (WTC) for a discussion on how technology has changed the landscape of hiking safety. Learn tips, tricks and tools that you can leverage to stay safe in the outdoors. Montrose SAR will share real-life examples of recent rescues and provide insight on what could be done to avoid or address those situations in real time. WTC will provide advice on what hikers can do to stay safe while hiking our local mountains both on and off the trail, using modern technology as well as traditional wilderness skills.",
+      "Join experts from Montrose Search & Rescue (SAR) and Sierra Club's Wilderness Travel Course (WTC) for a discussion on how technology has changed the landscape of hiking safety.",
     type: "Online",
     capacity: 20,
     price: 0.0,
@@ -19,7 +19,7 @@ const events = [
     venueId: 2,
     groupId: 2,
     name: "HOWLOWEEN Sip and Paint a Pet Portrait Fun- Barking Dog",
-    description: `What better way to celebrate the Fall than going to a dog friendly restaurant and a painting a portrait with your dog in Costume? It’s a chance to enjoy some fun good with food and drinks with your pup. Relax with your pup at Barking Dog restaurant on the Upper East Side in Manhattan New York City and Paint your Pet Portrait with personal Artist one on one instruction.`,
+    description: `What better way to celebrate the Fall than going to a dog friendly restaurant and a painting a portrait with your dog in Costume? It’s a chance to enjoy some fun good with food and drinks with your pup.`,
     type: "In person",
     capacity: 5,
     price: 0.0,
@@ -31,7 +31,7 @@ const events = [
     groupId: 3,
     name: "Let's visit Sleepy Hollow and Kykuit and Philips Manor",
     description:
-      "Hudson Valley is still full of fall colors. There aren't too many places that are synonyms with Halloween than Sleepy Hollow, the home of the Headless Horseman. Let's visit this charming town, check out the statue of the Headless Horseman and walk through the spooky Sleepy Hollow cemetery. Full of grandiose and opulent mausoleums that are home to many famous figures, including Washington Irving who wrote 'The Legend of Sleepy Hollow'. And other famous historical individuals including various Carnegie and Rockefeller family members. We will also tour Kykuit Estate, gardens and Philips Manor. This Rockefeller estate has been closed during the pandemic but is finally offering tours. With rich and troubled history, we will journey to Colonial America of 1750 where 23 enslaved individuals called Phillips Manor home. We will also have a nice dinner together after our visit and tour. We will meet in the city and travel together in a comfortable passenger van. The cost of the trip, including the Kykuit estate tour is $95",
+      "Hudson Valley is still full of fall colors. There aren't too many places that are synonyms with Halloween than Sleepy Hollow, the home of the Headless Horseman. ",
     type: "In person",
     capacity: 20,
     price: 30.0,
